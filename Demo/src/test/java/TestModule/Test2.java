d2b1f3256ccbf9c83e68dc1b4d51ee815984e841
RushikeshPatil1405 <rushikeshpatil1405@gmail.com>
2022-10-20 23:27:43 +0530

    Second Commit

---
+++ Demo/src/test/java/TestModule/Test2.java
@@ -0,0 +1,13 @@
+package TestModule;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Hello Everyone");
+		System.out.println("How are you");
+		
+	}
+
+}

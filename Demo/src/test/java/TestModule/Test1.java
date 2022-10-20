54289205e358aedd2043763a6c35104455a88538
RushikeshPatil1405 <rushikeshpatil1405@gmail.com>
2022-10-20 23:14:16 +0530

    First Commit

---
+++ Demo/src/test/java/TestModule/Test1.java
@@ -0,0 +1,12 @@
+package TestModule;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Everyone");
+		System.out.println("How are you");
+
+	}
+
+}
